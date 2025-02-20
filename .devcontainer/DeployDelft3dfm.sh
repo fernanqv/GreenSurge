c865135e12680636f60abe01de4847022a03bc60
fernanqv <fernanqv@users.noreply.github.com>
2025-02-20 07:20:50 +0100

    Create DeployDelft3dfm.sh

---
+++ .devcontainer/DeployDelft3dfm.sh
@@ -0,0 +1,3 @@
+# Download and install delft3dfm RPM
+wget http://personales.unican.es/fernanqv/delft3dfm-cli-hmwq_lnx64-2024.03-193.x86_64.rpm
+rpm -iv --force delft3dfm-cli-hmwq_lnx64-2024.03-193.x86_64.rpm
