f714296f2784a6db70103b4dc1bfc6c32f8be19b
fernanqv <fernanqv@users.noreply.github.com>
2025-02-20 06:53:18 +0100

    Create DeployIntelCompiler.sh

---
+++ .devcontainer/DeployIntelCompiler.sh
@@ -0,0 +1,18 @@
+# Create repo file
+tee > /tmp/oneAPI.repo << EOF
+[oneAPI]
+name=Intel® oneAPI repository
+baseurl=https://yum.repos.intel.com/oneapi
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://yum.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
+EOF
+
+mv /tmp/oneAPI.repo /etc/yum.repos.d
+
+# Install Intel oneapi compilers, mkl and mpi
+dnf install -y intel-oneapi-compiler-dpcpp-cpp 
+dnf install -y intel-oneapi-compiler-fortran
+dnf install -y intel-oneapi-mkl
+dnf install -y intel-oneapi-mpi
